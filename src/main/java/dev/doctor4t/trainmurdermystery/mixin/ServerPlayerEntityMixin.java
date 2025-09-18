2a61d7404d25e7de128b83f46a6a8935a0d328a7
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2025-09-18 19:58:25 +0100

    Update ServerPlayerEntityMixin.java

--- src/main/java/dev/doctor4t/trainmurdermystery/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/mixin/ServerPlayerEntityMixin.java
@@ -2,15 +2,20 @@ package dev.doctor4t.trainmurdermystery.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ServerPlayerEntity.class)
 public class ServerPlayerEntityMixin {
     @WrapOperation(method = "trySleep", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;sendMessage(Lnet/minecraft/text/Text;Z)V"))
-    public void tmm$disableSleepMessage(ServerPlayerEntity instance, Text message, boolean overlay, Operation<Void> original) {
+    public void tmm$disableSleepMessage(ServerPlayerEntity instance, Text message, boolean overlay, Operation<Void> original) {}
 
-    }
+    @WrapOperation(method = "trySleep", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;setSpawnPoint(Lnet/minecraft/registry/RegistryKey;Lnet/minecraft/util/math/BlockPos;FZZ)V"))
+    public void tmm$disableSetSpawnpoint(ServerPlayerEntity instance, RegistryKey<World> dimension, @Nullable BlockPos pos, float angle, boolean forced, boolean sendMessage, Operation<Void> original) {}
 }
